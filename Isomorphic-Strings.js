525ee932a038e729d1f0013c2ded1a38ba93a6fa
ChunyuBai <baichunyu26@gmail.com>
2023-02-23 11:56:15 -0500

    add a fix solution

--- Isomorphic-Strings.js
+++ Isomorphic-Strings.js
@@ -47,3 +47,19 @@ then return true, otherwise return false
       }
   } return true;
 };
+
+// Fixed solution: 
+
+ var isIsomorphic = function(s, t) {
+  if(s.length !== t.length){
+      return false
+  } 
+  let mapS = {}, mapT = {};
+  for (let i = 0; i < s.length; i++) {
+      if (!mapS[s[i]] && !mapT[t[i]]) {
+          mapS[s[i]] = t[i];
+          mapT[t[i]] = s[i];
+      } else if (mapS[s[i]] !== t[i] || mapT[t[i]] !== s[i]) return false;
+  }   
+  return true;
+};
